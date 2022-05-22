0feeb65767221f1f33db665a53735edd1a378249
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-22 22:33:12 +0600

    basic even numbers

--- while-loop.js
+++ while-loop.js
@@ -2,7 +2,7 @@
 let i = 0;
 while (i < 20) {
   i++;
-  console.log(i); // 1-20 output
+   console.log(i); // 1-20 output
 }
 
 // basic while loop odd numbers
@@ -11,4 +11,11 @@ let oddNum = 1;
 while (oddNum < 20) {
   console.log(oddNum); // output 1-3-5-7...19
   oddNum += 2;
+}
+
+// basic while loop even numbers
+let evenNum = 0;
+while (evenNum <= 20) {
+  console.log(evenNum); // output 0-2-4-6...20
+  evenNum += 2;
 }
