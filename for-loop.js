ea78c3923c1ca99c76cd637b5f23c97686d8d8c8
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-23 22:46:14 +0600

    get array elements in for loop

--- for-loop.js
+++ for-loop.js
@@ -4,10 +4,29 @@ for (let i = 0; i < 20; i++) {
 }
 // basic for loop odd numbers
 for (let oddNum = 1; oddNum < 20; oddNum += 2) {
+  // you can use "oddNum = oddNum + 2"
   console.log(oddNum);
 }
 
 // basic for loop even numbers
 for (let evenNum = 0; evenNum < 20; evenNum += 2) {
+  // you can use " evenNum = evenNum + 2"
   console.log(evenNum);
+}
+
+// get array elements in for loop
+const items = [
+  "laptop",
+  "mobail",
+  "phone",
+  "charger",
+  "headphone",
+  "keyboard",
+  "table",
+  "router machine",
+];
+
+for (getArrayItems = 0; getArrayItems < items.length; getArrayItems++) {
+  const arrayItem = items[getArrayItems];
+  console.log(arrayItem);
 }
