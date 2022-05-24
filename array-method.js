1be2853817c6579b9cf70ed571609f15983117cb
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-24 14:46:01 +0600

    array istructure and push and pop mehod

---
+++ array-method.js
@@ -0,0 +1,11 @@
+// basic array declare
+
+const phones = ["xiaomi","vivo","lava","oppo","realme","asus","huewei"];
+
+// you can push() and pop() in an array  
+phones.push("lg") //this keyword increases an element in an array of last items
+phones.push("Iphone")
+
+phones.pop() //this keyword decreases an element in an array of last items
+
+console.log(phones);
