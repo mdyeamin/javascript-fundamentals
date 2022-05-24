6e0108cb799e8a8f5a2fb40e4b646e30f4827c76
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-24 15:01:36 +0600

    find a element in an array with 'indexOf' keyword

---
+++ array-method-push-pop-shift-unshift-indexOf.js
@@ -0,0 +1,24 @@
+// basic array declare
+
+const phones = ["xiaomi", "vivo", "lava", "oppo", "realme", "asus", "huewei"];
+
+// you can push() and pop() in an array
+phones.push("lg"); //this keyword increases an element in an array of last items
+phones.push("Iphone");
+
+phones.pop(); //this keyword decreases an element in an array of last items
+
+console.log(phones);
+/* 
+shift() and unshift() method will be increases and decreases the first items
+
+you can use push and pop like and see output
+*/
+
+// find a element in an array with "indexOf" keyword
+const phoneName = "'Iphone'";
+if (phones.indexOf(phoneName) != -1) {
+  console.log("xiaomi phones");
+} else {
+  console.log(`sorry ${phoneName} didn't match`);
+}
