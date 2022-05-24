3aa0d0174fe0cde298ee2b3604c447d74b014c4f
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-24 10:12:34 +0600

    continue method

--- loop-break-and-continue.js
+++ loop-break-and-continue.js
@@ -1,10 +1,40 @@
-// while loop break and continue 
+// while loop break and continue
 
 let i = 0;
-while(i<10){
-    if(i==3){
-        break; // this is break methon
-    }
-console.log(i);
-    i++
+while (i < 10) {
+  if (i == 3) {
+    break; // this is break method
+  }
+  console.log(i);
+  i++;
+} 
+
+let loopContinue = 0;
+while (loopContinue < 10) {
+    loopContinue++;
+  if (loopContinue % 2 == 0) {
+    continue; // this is continue method
+  }
+  console.log(loopContinue);
+}
+ 
+// continue advance method
+const items = [
+  "laptop",
+  "mobail",
+  "phone",
+  "charger",
+  "headphone",
+  "keyboard",
+  "table",
+  "router machine",
+];
+let skipArrayItems = 0;
+while (skipArrayItems < items.length) {
+  const skipArrayItem = items[skipArrayItems];
+  skipArrayItems++;
+  if (skipArrayItem.includes("l")) {
+    continue;
+  }
+  console.log(skipArrayItem);
 }
