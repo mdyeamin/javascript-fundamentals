08da3178f3b39678fc17f790dc20334f11e499c8
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-25 09:51:03 +0600

    remove duplicate element prom array

--- removeDuplicateElementFromArray.js
+++ removeDuplicateElementFromArray.js
@@ -1,4 +1,4 @@
 // remove duplicate element prom array
 const numbers = [20, 22, 10, 20, 39, 10, 30];
-const uniqueNumbers = [];
-console.log(...new Set(numbers));
+const uniqueNumbers = [...new Set(numbers)];
+console.log(uniqueNumbers);
