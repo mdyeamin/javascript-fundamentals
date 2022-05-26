d0d2f68816932ba03d67f6e923c0f50d87b821de
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-26 22:43:09 +0600

    factorial with a function

--- factorial.js
+++ factorial.js
@@ -1,6 +1,15 @@
+// 1st method factorial with for loop
 let factorial = 1;
 for (let i = 1; i < 8; i++) {
-//   console.log(i);
+  //   console.log(i);
   factorial *= i;
 }
 console.log(factorial);
+
+// 2nd method factorial with a function
+function factorialNum(n) {
+  if (n < 0) return;
+  if (n < 2) return 1;
+  return n * factorialNum(n - 1);
+}
+console.log(factorialNum(7));
