f066f0af0e894eee0ac55059757c8339a40d2ca9
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-27 15:31:11 +0600

    decrementFactorialWithWhileLoop

--- factorial.js
+++ factorial.js
@@ -1,4 +1,4 @@
-/* // 1st method factorial with for loop
+// 1st method factorial with for loop
 let factorial = 1;
 for (let i = 1; i < 8; i++) {
   //   console.log(i);
@@ -26,7 +26,7 @@ function factorialNum(n) {
 }
 console.log(factorialNum(7));
 
- */
+
 // 4th method Factorial using a while loop or a decrementing loop
 function decrementFactorialWithWhileLoop(num) {
   let fac = 1;
