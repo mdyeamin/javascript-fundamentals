78fc54293a595d52839a3ca024782dd63aef2f01
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-27 22:12:24 +0600

    Math.(ceil)

--- Math.js
+++ Math.js
@@ -1,8 +1,14 @@
 // Returns the absolute value of abs number
+// Math method 1
 const abs = -4;
 const absolute = Math.abs(abs);
 console.log(absolute);
 
-const acos = -2; // arccosine method number within the range of 1 and -1. ১ থেকে -১ পর্যন্ত এটার রেঞ্জ।
- const arccosine = Math.acos(acos);
+// Math mathod 2
+const ceil = 7.30002;
+const sealing = Math.ceil(ceil);
+console.log(sealing);
+
+const acos = -1; // arccosine method number within the range of 1 and -1. ১ থেকে -১ পর্যন্ত এটার রেঞ্জ।
+const arccosine = Math.acos(acos);
 console.log(arccosine);
