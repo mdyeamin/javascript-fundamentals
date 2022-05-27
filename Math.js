455e5f9bfa3ec02497e72639962223d407deca07
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-27 22:20:12 +0600

    Math.Round()

--- Math.js
+++ Math.js
@@ -12,7 +12,12 @@ console.log(sealing);
 // Math method 3
 const floor = 7.30002;
 const flooring = Math.floor(floor);
-console.log( flooring);
+console.log(flooring);
+
+// Math method 4
+const round = 5.4099;
+const rounding = Math.round(round);
+console.log(rounding);
 
 const acos = -1; // arccosine method number within the range of 1 and -1. ১ থেকে -১ পর্যন্ত এটার রেঞ্জ।
 const arccosine = Math.acos(acos);
