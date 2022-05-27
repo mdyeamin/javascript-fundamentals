524f916641fc80f1822b9916abe95ded227d26b7
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-27 22:03:30 +0600

    Math.acos number renge 1 to -1

--- Math.js
+++ Math.js
@@ -1,4 +1,8 @@
-// Returns the absolute value of number
-const x = -4;
-const absolute = Math.abs(x);
+// Returns the absolute value of abs number
+const abs = -4;
+const absolute = Math.abs(abs);
 console.log(absolute);
+
+const acos = -2; // arccosine method number within the range of 1 and -1. ১ থেকে -১ পর্যন্ত এটার রেঞ্জ।
+ const arccosine = Math.acos(acos);
+console.log(arccosine);
