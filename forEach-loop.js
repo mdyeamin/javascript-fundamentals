e87cd8c5f510da53abecb79a4c379a6398859d0b
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-28 15:07:04 +0600

    foreach esc

--- forEach-loop.js
+++ forEach-loop.js
@@ -5,8 +5,8 @@ use case-  array.forEach(function())
 // basic forEach structure
 const num = [10, 22, 34, 20, 55, 76, 44, 24, 33, 87];
 
-num.forEach(forEachFunction);
 //method 1
+num.forEach(forEachFunction);
 function forEachFunction(num) {
   // here I declare the below function and call of a forEach loop
   console.log(num);
@@ -15,16 +15,17 @@ function forEachFunction(num) {
 num.forEach(function (num) {
   // The rules for writing functions within forEach
   console.log(num);
-}); 
+});
 //method 3
 const forEach = (num) => {
-    console.log(num);
+  console.log(num);
 };
 num.forEach(forEach);
 
 //method 4
-num.forEach((num) => {
-  // es6 function
-  console.log(num);
-});
+num.forEach((num) =>
+  
+  console.log(num)
+);
+
 
