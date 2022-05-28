8215762d4bf4b502d45e442a72cb22a258cffca3
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-28 11:13:23 +0600

    EulersNumbers

--- Math.js
+++ Math.js
@@ -1,4 +1,4 @@
-/* // Returns the absolute value of abs number
+// Returns the absolute value of abs number
 // Math method 1
 const abs = -4;
 const absolute = Math.abs(abs);
@@ -24,7 +24,7 @@ const acos = -1; // arccosine method number within the range of 1 and -1. à§§ à¦
 const arccosine = Math.acos(acos);
 console.log(arccosine);
 
- */
+
 // Math method 6
 function EulersNumbers(e) {
   return Math.E;
@@ -46,4 +46,31 @@ function compoundOneYear(interestRate, currentVal) {
   
   console.log(compoundOneYear(0.05, 100));
   // expected output: 105.12710963760242
-  
+ 
+ 
+  /* abs(x)	Returns the absolute value of x
+  acos(x)	Returns the arccosine of x, in radians
+  acosh(x)	Returns the hyperbolic arccosine of x
+  asin(x)	Returns the arcsine of x, in radians
+  asinh(x)	Returns the hyperbolic arcsine of x
+  atan(x)	Returns the arctangent of x as a numeric value between -PI/2 and PI/2 radians
+  atan2(y, x)	Returns the arctangent of the quotient of its arguments
+  atanh(x)	Returns the hyperbolic arctangent of x
+  cbrt(x)	Returns the cubic root of x
+  ceil(x)	Returns x, rounded upwards to the nearest integer
+  cos(x)	Returns the cosine of x (x is in radians)
+  cosh(x)	Returns the hyperbolic cosine of x
+  exp(x)	Returns the value of Ex
+  floor(x)	Returns x, rounded downwards to the nearest integer
+  log(x)	Returns the natural logarithm (base E) of x
+  max(x, y, z, ..., n)	Returns the number with the highest value
+  min(x, y, z, ..., n)	Returns the number with the lowest value
+  pow(x, y)	Returns the value of x to the power of y
+  random()	Returns a random number between 0 and 1
+  round(x)	Rounds x to the nearest integer
+  sin(x)	Returns the sine of x (x is in radians)
+  sinh(x)	Returns the hyperbolic sine of x
+  sqrt(x)	Returns the square root of x
+  tan(x)	Returns the tangent of an angle
+  tanh(x)	Returns the hyperbolic tangent of a number
+  trunc(x)	Returns the integer part of a number (x) */
