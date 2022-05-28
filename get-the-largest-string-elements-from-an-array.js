e5a5a3a565343c1514a7ca881eb22b8496b89d58
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-28 21:39:27 +0600

    largest arrow function

--- get-the-largest-string-elements-from-an-array.js
+++ get-the-largest-string-elements-from-an-array.js
@@ -1,14 +1,40 @@
 // first of all, i write a forEach loop function
+
+// raw fanction
 function getArrayLargestElement(array) {
   let largestElement = "";
-  array.forEach(function (word) {
+  array.forEach(largest);
+  function largest(word) {
     if (word.length > largestElement.length) {
       largestElement = word;
     }
-  });
+  }
   return largestElement;
 }
 
-const a = ["amader", "sonar", "bangladesh", "ami tomay", "onek ", "valo basi"];
+const banglaGit = [
+  "amader",
+  "sonar",
+  "bangladesh",
+  "ami tomay",
+  "onek ",
+  "valo basi",
+];
 
-console.log(getArrayLargestElement(a));
+console.log(getArrayLargestElement(banglaGit));
+
+// es6 largest with forEach lopp arrow function
+
+const getLargestArrowFunc = (array) => {
+  let largest = "";
+  array.forEach((word) => {
+    if (word.length > largest.length) {
+      largest = word;
+    }
+  });
+  return largest;
+};
+
+console.log(
+  getLargestArrowFunc(["hello", "world", "md yeamin", "i'm", "here"])
+);
