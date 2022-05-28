fbad6b8e6391e2935ec73c295c9ff422a5c07141
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-28 14:23:44 +0600

    basic forEach structure

---
+++ forEatch-loop.js
@@ -0,0 +1,10 @@
+/* 
+The forEatch loop receive a function.
+use case-  array.forEatch(function())
+ */
+// basic forEach structure
+const num = [10, 22, 34, 20, 55, 76, 44, 24, 33, 87];
+
+num.forEach(function (num) {
+  console.log(num);
+});
