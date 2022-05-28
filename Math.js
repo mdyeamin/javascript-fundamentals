29255e7a56b1daa4f9adf3b1342c350f0b65caa5
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-28 10:16:30 +0600

    EulersNumbers

--- Math.js
+++ Math.js
@@ -30,3 +30,20 @@ function EulersNumbers(e) {
   return Math.E;
 }
 console.log(EulersNumbers());
+
+// Description
+// Because E is a static property of Math, you always use it as Math.E, rather than as a property of a Math object you created (Math is not a constructor).
+
+function compoundOneYear(interestRate, currentVal) {
+    return currentVal * (Math.E ** interestRate);
+  }
+  
+  console.log(Math.E);
+  // expected output: 2.718281828459045
+  
+  console.log((1 + (1 / 1000000)) ** 1000000);
+  // expected output: 2.718280469 (approximately)
+  
+  console.log(compoundOneYear(0.05, 100));
+  // expected output: 105.12710963760242
+  
