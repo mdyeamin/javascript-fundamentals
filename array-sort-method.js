97f00536a9ed479bec6c292e5c28b042890b3a9b
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-29 21:50:44 +0600

    modified sort

--- array-sort-method.js
+++ array-sort-method.js
@@ -1,15 +1,19 @@
 const arraySort = [2, 4, 3, 20, 10, 60, 7];
 
-console.log(arraySort.sort(arrayABSort));
+arraySort.sort(arrayABSort);
 
 function arrayABSort(a, b) {
   return a - b;
 }
 
-console.log(
-  arraySort.sort(function (a, b) {
-    return a - b;
-  })
-);
+console.log(arraySort);
 
-console.log(arraySort.sort((a, b) => a - b));
+arraySort.sort(function (a, b) {
+  return a - b;
+});
+
+console.log(arraySort);
+
+arraySort.sort((a, b) => a - b);
+
+console.log(arraySort);
