ab10ccb2eb1d4dea6d4237dbfb5cab9b5184e274
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-30 19:42:42 +0600

    largest negative number an array

--- get-the-largest-number-from-an-array.js
+++ get-the-largest-number-from-an-array.js
@@ -3,7 +3,7 @@
 const a = [3, 55, 6, 9, 11, 65, 97, 4];
 console.log(Math.max(a)); // output will be NaN */
 //if you try this way, don't work the program
-var num = [4, 5, 1, 3];
+/* var num = [4, 5, 1, 3];
 console.log(Math.max(num)); // logs NaN
 console.log(Math.max(1, 3, 4, 2)); //work only on the console without null
 
@@ -15,13 +15,12 @@ console.log(Math.max.apply(null, a)); // null must be used otherwise code doesn'
 const arrNum = [4, 3, 1, 8, 55];
 console.log(Math.max(...arrNum));
 
-
 // 3rd yaw array largest number with a function
 function largestElement(array) {
   let bigest = 0;
   for (let i = 0; i < array.length; i++) {
     if (bigest < array[i]) {
-        let arrayIndex = array[i]
+      let arrayIndex = array[i];
       bigest = arrayIndex;
     }
   }
@@ -32,3 +31,19 @@ const biggestAge = [22, 34, 56, 78, 67];
 
 const getLargest = largestElement(biggestAge);
 console.log(getLargest);
+ */
+function largestNegativeElement(array) {
+  let bigest = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (bigest < array[i]) {
+      let arrayIndex = array[i];
+      bigest = arrayIndex;
+    }
+  }
+  return bigest;
+}
+
+const biggestnegativeAge = [-22, -34, -56, -78, -67];
+
+const getNegativeLargest = largestNegativeElement(biggestnegativeAge);
+console.log(getNegativeLargest);
