eecf9d4da4a90711dfd028c72cb1d065568facdc
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-30 13:06:43 +0600

    sum-of-all-numbers-in-tow-array

--- sum-of-all-numbers-in-an-array.js
+++ sum-of-all-numbers-in-an-array.js
@@ -1,4 +1,4 @@
-const arr = [23, 44, 53, 21, 22, 87, 10];
+/* const arr = [23, 44, 53, 21, 22, 87, 10];
 let sum = 0;
 for (let i = 0; i < arr.length; i++) {
   sum = sum + arr[i];
@@ -14,4 +14,16 @@ function getAnArraySum(array) {
 }
 const funcArray = [234, 56, 7, 45, 56, 78];
 const sumArray = getAnArraySum(funcArray);
-console.log(sumArray);
+console.log(sumArray); */
+
+function towArraySum(arr1, arr2) {
+  let sum = 0;
+  for (let i = 0; i < arr1.length && arr2.length; i++) {
+    sum = arr1[i] + arr2[i];
+    console.log(sum);
+  }
+}
+
+const a = [34, 56, 7, 7];
+const b = [10, 20, 30, 40, 88];
+towArraySum(a, b);
