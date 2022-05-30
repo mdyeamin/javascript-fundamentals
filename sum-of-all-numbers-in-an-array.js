bc55213762927143c8931d96e011a24d4826def6
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-30 19:45:25 +0600

    uncomment

--- sum-of-all-numbers-in-an-array.js
+++ sum-of-all-numbers-in-an-array.js
@@ -1,4 +1,4 @@
-/* const arr = [23, 44, 53, 21, 22, 87, 10];
+ const arr = [23, 44, 53, 21, 22, 87, 10];
 let sum = 0;
 for (let i = 0; i < arr.length; i++) {
   sum = sum + arr[i];
@@ -14,7 +14,7 @@ function getAnArraySum(array) {
 }
 const funcArray = [234, 56, 7, 45, 56, 78];
 const sumArray = getAnArraySum(funcArray);
-console.log(sumArray); */
+console.log(sumArray); 
 
 function towArraySum(arr1, arr2) {
   let sum = 0;
