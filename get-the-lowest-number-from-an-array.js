35464398d5bd30872fd39d85506fbd0b2053c230
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-30 19:58:08 +0600

    get-the-largest-number-from-an-array

--- get-the-lowest-number-from-an-array.js
+++ get-the-lowest-number-from-an-array.js
@@ -1,3 +1,23 @@
+/*   get the largest element from an array
+
+const a = [3, 55, 6, 9, 11, 65, 97, 4];
+console.log(Math.min(a)); // output will be NaN */
+//if you try this way, don't work the program
+var num = [4, 5, 1, 3];
+console.log(Math.min(num)); // logs NaN
+console.log(Math.min(1, 3, 4, 2)); //work only on the console without null
+
+
+// 1st method array lowest number
+const a = [3, 55, 6, 9, 11, 65, 97, 4];
+console.log(Math.min.apply(null, a)); // null must be used otherwise code doesn't work
+
+// 2nd method
+const arrNum = [4, 3, 1, 8, 55];
+console.log(Math.min(...arrNum));
+
+
+
 function lowestElement(array) {
     let lowest = array[0];
     for (let i = 0; i < array.length; i++) {
