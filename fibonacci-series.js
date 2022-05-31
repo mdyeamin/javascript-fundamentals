d40dc021b54244e886457bec6a6eff714f016744
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-31 14:31:14 +0600

    fibo with function

--- fibonacci-series.js
+++ fibonacci-series.js
@@ -1,6 +1,15 @@
 let fibo = [0, 1];
-for (let i = 2; i <= 19; ) {
+for (let i = 2; i <= 10; i++) {
   fibo[i] = fibo[i - 1] + fibo[i - 2];
-  i++;
 }
 console.log(fibo);
+
+function fiboSeries(num) {
+  let fibo = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+  }
+  return fibo;
+
+}
+console.log(fiboSeries(20));
