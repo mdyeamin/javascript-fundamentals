ed491606fcbd9a54f72f34b21250b8462842ad79
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-31 14:43:39 +0600

    fibo with recursuve

--- fibonacci-series.js
+++ fibonacci-series.js
@@ -1,15 +1,25 @@
-let fibo = [0, 1];
+/* let fibo = [0, 1];
 for (let i = 2; i <= 10; i++) {
   fibo[i] = fibo[i - 1] + fibo[i - 2];
 }
 console.log(fibo);
-
+//  with a function
 function fiboSeries(num) {
   let fibo = [0, 1];
   for (let i = 2; i <= num; i++) {
     fibo[i] = fibo[i - 1] + fibo[i - 2];
   }
   return fibo;
-
 }
 console.log(fiboSeries(20));
+ */
+// with recursive funcrion
+
+function recursiveFibo(num) {
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+
+  return recursiveFibo(num - 2) + recursiveFibo(num - 1);
+}
+
+console.log(recursiveFibo(10));
