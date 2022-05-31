89577d9f912554fd3df26433f0a9b06b701af852
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-31 21:34:02 +0600

    multiple dandom number with for lppo

---
+++ Math.random-number-duplicate-remove-from-array.js
@@ -0,0 +1,12 @@
+// const random = Math.random();
+// const random10 = random * 100;
+// // const intRandom = parseInt(random10);
+// const intRandom = Math.round(random10);
+
+for (let i = 0; i < 5 * 2; i++) {
+  const random = Math.random();
+  const random10 = random * 100;
+  // const intRandom = parseInt(random10);
+  const round = Math.round(random10);
+  console.log(round);
+}
