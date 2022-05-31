53e7760fa676bf8e5d6a06c9f244da451236e526
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-31 23:39:34 +0600

    remove duplicate math.random number

--- Math.random-number-duplicate-remove-from-array.js
+++ Math.random-number-duplicate-remove-from-array.js
@@ -3,10 +3,17 @@
 // // const intRandom = parseInt(random10);
 // const intRandom = Math.round(random10);
 
+let removeDuplicateRandom = [];
 for (let i = 0; i < 5 * 2; i++) {
   const random = Math.random();
   const random10 = random * 100;
   // const intRandom = parseInt(random10);
   const round = Math.round(random10);
-  console.log(round);
+
+  if (removeDuplicateRandom.indexOf(round) == -1) {
+    removeDuplicateRandom.push(round);
+  } else {
+    console.log("get duplicate num is ", round);
+  }
 }
+console.log(removeDuplicateRandom);
