ed4edd8234c0013181d785319f3c61b8747b080a
Md Yeamin <ahmedyeamin05@gmail.com>
2022-05-31 14:29:36 +0600

    add recursive

--- factorial.js
+++ factorial.js
@@ -15,10 +15,22 @@ function factNum(num) {
   }
   return fact;
 }
-
-
 console.log('fact number is', factNum(7));
-// 3rd method factorial with a function
+
+
+// 3rd recursive 
+function factRecursiveNum(num) {
+  
+  if (num === 0) {
+    return 1;
+  }
+  return num * factRecursiveNum(num - 1);
+}
+console.log("fact number is", factRecursiveNum(7));
+
+//* * 
+
+// 4th method factorial with a recursive function 
 function factorialNum(n) {
   if (n < 0) return;
   if (n < 2) return 1;
@@ -27,7 +39,7 @@ function factorialNum(n) {
 console.log(factorialNum(7));
 
 
-// 4th method reverse Factorial using a while loop or a decrementing loop
+// 5th method reverse Factorial using a while loop and a decrementing loop
 function decrementFactorialWithWhileLoop(num) {
   let fac = 1;
   let i = num;
@@ -39,3 +51,4 @@ function decrementFactorialWithWhileLoop(num) {
   return fac;
 }
 console.log(decrementFactorialWithWhileLoop(7));
+ 
