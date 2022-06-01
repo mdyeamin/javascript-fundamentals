380f824d7952f3d59a6c5d45c65e06090471e283
Md Yeamin <ahmedyeamin05@gmail.com>
2022-06-01 13:53:59 +0600

    array duplicate element store a different array

--- remove-duplicate-element-from-array.js
+++ remove-duplicate-element-from-array.js
@@ -29,11 +29,13 @@ console.log(removeDups(birds));
 
 const arr = [33, 44, 645, 33, 77, 86, 77, 10, 19, 10];
 let removeDup = [];
+const dup = [];
 for (let i = 0; i < arr.length; i++) {
   if (removeDup.indexOf(arr[i]) == -1) {
     removeDup.push(arr[i]);
   } else {
-    console.log("duo", arr[i]);
+    console.log("dup", dup.push(arr[i]));
   }
 }
 console.log(removeDup);
+console.log("this is duplicate number", dup);
