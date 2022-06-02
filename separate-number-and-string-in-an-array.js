428a4865eefea813fdba91e85bff79b028f0abbb
Md Yeamin <ahmedyeamin05@gmail.com>
2022-06-02 14:32:02 +0600

    string and number element To separate from the array

---
+++ separate-number-and-string-in-an-array.js
@@ -0,0 +1,24 @@
+const arr = [
+  103,
+  "this",
+  49,
+  "is",
+  33,
+  "a",
+  19,
+  "javascript",
+  54,
+  87,
+  "programs",
+];
+let str = [""];
+let num = [];
+for (let i = 0; i < arr.length; i++) {
+  if (typeof arr[i] === "number") {
+    num.push(arr[i]);
+} else {
+    str = str + arr[i] + " ";
+}
+}
+console.log(num);
+console.log(str);
