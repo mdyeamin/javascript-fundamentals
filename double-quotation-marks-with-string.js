5b75c107b274a4ddd0608f6ec05f868ae46acd2b
Md Yeamin <ahmedyeamin05@gmail.com>
2022-06-03 21:43:41 +0600

    code output with forward sales

--- double-quotation-marks-with-string.js
+++ double-quotation-marks-with-string.js
@@ -1,5 +1,21 @@
 // double in the  quotation between you can use double quotes with \ symbol
 
-let doubleQuots = "this is a \"double quotation\" marks"
+let doubleQuots = "this is a \"double quotation\" marks";
 
 console.log(doubleQuots);
+
+const a = "amade \\ code and \f cpde";
+console.log(a);
+
+// escape sequence in string
+/* 
+code output 
+\' single coute
+\" double coute
+\\ backslash
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+*/
