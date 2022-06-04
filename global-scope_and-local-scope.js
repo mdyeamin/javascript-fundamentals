55a2445b48fe169f245fed927a31d7cf835e8544
Md Yeamin <ahmedyeamin05@gmail.com>
2022-06-04 13:01:50 +0600

    local VS global scope

--- global-scope_and-local-scope.js
+++ global-scope_and-local-scope.js
@@ -4,7 +4,6 @@
 // local scope ***
 //  Local Scope occurs when you create a variable inside a function. By doing that, the visibility and accessibility of the variable is only allowed within that function. Any variable created inside the yellow box is a local variable, just like any variable inside blue box is a global one.
 
-
 const color = "blue"; // this is global variable, you can everywhere use
 
 const returnSkyColor = () => {
@@ -13,14 +12,42 @@ const returnSkyColor = () => {
 
 console.log(returnSkyColor()); // blue
 
+// global scope
+
+const myGlobal = 10;
+
+const oppsGlobal = 7;
+function func() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += " Global is " + myGlobal;
+  }
+  if (typeof oppsGlobal != "undefined") {
+    output += " oppsGlobal is " + oppsGlobal;
+  }
+  console.log(output);
+}
+
+func();
 
 // program showing local scope of a variable
 let a = "hello";
 
 function greet() {
-    let b = "World"
-    console.log(a + b);
+  let b = "World"; //you can't use this variable outside of the function
+  console.log(a + b);
 }
 
 greet();
 console.log(a + b); // error
+
+// global VS local
+
+const outerWear = "T-shirt"; // this variable is a global scope
+
+function outFit() {
+  const outerWear = "sweater"; // this variable is a local scope
+  return outerWear;
+}
+
+console.log(outFit());
