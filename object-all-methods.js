6788fe64bf4c289ca626ca2d402bed465300264a
Md Yeamin <ahmedyeamin05@gmail.com>
2022-06-04 22:43:57 +0600

    delete properties form object

---
+++ object-all-methods.js
@@ -0,0 +1,26 @@
+// basic object declare
+const computer = {
+  name: "hp",
+  cpu: "i7 11 gen",
+  condition: "new",
+  price: 58000,
+  color: "gray yellow",
+};
+// Object value get and change method:
+computer.price = 60000;
+computer["name"] = "dell"; // 1st method is an object value get and change
+const computercolor = "color"; // 2nd method
+computer[computercolor] = "gray black"; // 3rd method
+
+computer.isPowerFull = true; //1st way : add new properties to object
+computer["isHot"] = false; // 2nd way : add new properties to object
+computer["RAM"] = "64 GB";
+computer["SSD"] = "1 TB";
+
+computer["substance"] = "iron"; //this property added to for delete
+console.log(computer);
+
+// delete properties form object
+
+delete computer.substance;
+console.log(computer);
