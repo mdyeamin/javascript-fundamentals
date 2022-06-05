e0803a8a75aebc1b2e2fa12a98c12bbfaefe4f5f
Md Yeamin <ahmedyeamin05@gmail.com>
2022-06-05 09:30:32 +0600

    manipulating-complex-objects-and-accessing-values

---
+++ manipulating-complex-objects-and-accessing-values.js
@@ -0,0 +1,21 @@
+const myMusic = [
+  {
+    artist: "sammy sone",
+    title: "voice tone",
+    release_year: 2007,
+    formats: ["cd", "8T", "LP", "RIP", "until 16D"],
+    gold: "true",
+    team: {
+      learBbew: "sicago",
+      freeVoice: "japan",
+    },
+  },
+  {
+    artist: "Beau lewn",
+    title: "cereal man",
+    release_year: 1997,
+    formats: ["youtube video", "music box"],
+  },
+];
+
+console.log(myMusic[0].team.learBbew); // accesing manipulating objects values
