cb72a9eaae131b656152622ced9f23e44ef47017
Md Yeamin <ahmedyeamin05@gmail.com>
2022-06-07 12:40:51 +0600

    find expensive phone

--- find-cheapest-or-expensive-item-from-arrayOfObject.js
+++ find-cheapest-or-expensive-item-from-arrayOfObject.js
@@ -2,12 +2,19 @@ const phones = [
   {name: "huawei", model: "p20 pro", price: 89000, RAM: "8GB", SSD: "128GB"},
   {name: "lava", model: "lo12", price: 12000, RAM: "2GB", SSD: "16GB"},
   {name: "iPhone", model: "8 plus", price: 49000, RAM: "8GB", SSD: "256GB"},
-  {name: "samsung", model: "s 22 ultra", price: 149000, RAM: "12GB", SSD: "256GB"},
+  {
+    name: "samsung",
+    model: "s 22 ultra",
+    price: 149000,
+    RAM: "12GB",
+    SSD: "256GB",
+  },
   {name: "nokia", model: "1200", price: 1200, RAM: "512MB", SSD: "120MB"},
   {name: "nokia", model: "n50", price: 18900, RAM: "6GB", SSD: "128GB"},
   {name: "xiaomi", model: "note 10", price: 22000, RAM: "8GB", SSD: "128GB"},
-
 ];
+
+// find cheapest phone
 
 let cheapest = phones[0];
 for (const phone of phones) {
@@ -16,3 +23,13 @@ for (const phone of phones) {
   }
 }
 console.log(cheapest);
+
+// find expensive phone
+
+let expensive = phones[0];
+for (const phone of phones) {
+  if (expensive.price < phone.price) {
+    expensive = phone;
+  }
+}
+console.log(expensive);
