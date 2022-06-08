1e9de57a1b40889ec9ffd567eab9a8d38b20850b
Md Yeamin <ahmedyeamin05@gmail.com>
2022-06-08 22:47:20 +0600

    a problem task write

---
+++ travel-cost-siare-lower-for-reserved-long_distance-vehicles.js
@@ -0,0 +1,11 @@
+// problem task requirement bellow
+
+/* 
+write a function -
+1 I need a function where traveling will have a calculation
+2 This calculation will depend on the distance
+3 15 rupees per kilometer for the first 90 kilometers
+4 and 12 rupees per kilometer for the next 120 kilometers
+5 and 8 rupees per kilometer for the next kilometers
+*/
+
